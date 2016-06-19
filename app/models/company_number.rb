3851b05a3681d5f5d777711e34fe7dc4383814c9
Julien Malherbe <julien@malherbe.net>
2016-06-19 18:11:32 +0200

    annotate existing models

--- app/models/company_number.rb
+++ app/models/company_number.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: company_numbers
+#
+#  id           :integer          not null, primary key
+#  sip_endpoint :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class CompanyNumber < ActiveRecord::Base
 end
--- app/models/user_number.rb
+++ app/models/user_number.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: user_numbers
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  sip_endpoint :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class UserNumber < ActiveRecord::Base
   belongs_to :user
 end
